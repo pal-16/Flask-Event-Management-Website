411d94b57da0a5efc8bc27a8442e973d79232942
argus-eyed <mounilshah271@gmail.com>
2019-06-08 14:01:56 +0530

    Main jaa raha hoon khaana khaane. Wapas nahi aaunga.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-posts = [
+'''posts = [
     {
         'author': 'Corey Schafer',
         'title': 'Blog Post 1',
@@ -16,16 +16,26 @@ posts = [
     }
 ]
 
-
+'''
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
 
-
+'''
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+'''
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 
 if __name__ == '__main__':
