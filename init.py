8ee8ac8c7c36c6333623c96b674f08813eab7f03
argus-eyed <mounilshah271@gmail.com>
2019-06-08 13:55:45 +0530

    Ab toh ghar jaane do

--- init.py
+++ init.py
@@ -1,3 +1,6 @@
+from flask import SQLAlchemy
+from flask import render_template, url_for,Flask
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
