a0354c63a33f62c9a0167ed7bbefd5cb80f0d4d3
pal-16 <mantrypalak@gmail.com>
2019-06-17 14:48:25 +0530

    just a start

--- forms.py
+++ forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-
+'''
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
@@ -12,11 +12,28 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-
+'''
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
+    '''password = PasswordField('Password', validators=[DataRequired()])
+                remember = BooleanField('Remember Me')'''
     submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+    name = StringField('name')
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    
+    spaceused  = StringField('space used for')
+    location  = StringField('location')
+    price  = StringField('price')
+    details = StringField('details')
+
+    submit = SubmitField('Sign Up')
+    
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for
 from init import app, db
 import os
 from sqlalchemy.orm import Session
-from forms import RegistrationForm
+from forms import RegistrationForm,LoginForm
 from models import User
 
 '''posts = [
@@ -26,22 +26,50 @@ from models import User
 def home():
     return render_template('home.html')
 
-'''
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
+@app.route("/loginn", methods=['GET', 'POST'])
+def loginn():
+    
+      form = LoginForm()
+      
+           
+      return render_template('loginn.html', title='loginn', form=form)
 
-'''
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    
+    return render_template('i.html')
+
+@app.route("/uregister", methods=['GET', 'POST'])
+def uregister():
     form = RegistrationForm()
     if form.validate_on_submit():
 
-        user = User(username = form.username.data,email=form.email.data,password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        print("validated")
-
-        '''flash(f'Account created!', 'success')'''
+            user = User(username = form.username.data,email=form.email.data,password=form.password.data,spaceused=form.spaceused.data,location=form.location.data,price=form.price.data)
+            db.session.add(user)
+            db.session.commit()
+            print("validated")
     return render_template('temp.html', title='Register', form=form)
+
+@app.route("/pregister", methods=['GET', 'POST'])
+def pregister():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+
+            user = User(username = form.username.data,email=form.email.data,password=form.password.data,details=form.details.data,location=form.location.data,price=form.price.data)
+            db.session.add(user)
+            db.session.commit()
+            print("validated")
+    return render_template('dec.html', title='decregister', form=form)
+
+@app.route("/qregister", methods=['GET', 'POST'])
+def qregister():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+           
+            user = User(username = form.username.data,email=form.email.data,password=form.password.data,spaceused=form.spaceused.data,location=form.location.data,price=form.price.data)
+            db.session.add(user)
+            db.session.commit()
+            print("validated")
+    return render_template('cateror.html', title='catRegister', form=form)
+           
