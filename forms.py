905055a9711ba8e0e6fa039195e6823e37f0f7c5
argus-eyed <mounilshah271@gmail.com>
2019-08-07 16:33:38 +0530

    Incomplete changes. Work still in progress.

--- forms.py
+++ forms.py
@@ -9,33 +9,33 @@ from models import Org,User
 
 
 class LoginForm(FlaskForm):
-    email = EmailField('Email address', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')      
+    email = EmailField('Email address:', validators=[DataRequired(), Email()])
+    password = PasswordField('Password:', validators=[DataRequired()])
+    remember = BooleanField('Remember Me?')      
     submit = SubmitField('Login')
 
 
 class dynamicForm(FlaskForm):
-    price  = StringField('change price range')
-    location  = StringField(' change location')
-    remember = BooleanField('Remember Me')      
+    price  = StringField('Change price range:')
+    location  = StringField(' Change location:')
+    remember = BooleanField('Remember Me?')
     submit = SubmitField('Login')
 
 class hRegistrationForm(FlaskForm):
-    name = StringField(' hall name', validators=[DataRequired()])
-    email = EmailField('Email address', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
+    name = StringField('Hall name:', validators=[DataRequired()])
+    email = EmailField('Email address:', validators=[DataRequired(), Email()])
+    password = PasswordField('Password:', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password:',
                                      validators=[EqualTo('password')])
     
-    occasion  = StringField('space used for')
-    location  = SelectField('location',choices=[('a','andheri'),('z','any'),('b','borivali'),('c','churchgate'),('d','dadar'),('g','ghatkopar'),('j','jogeshwari'),('k','kandivali'),('l','lowerparel'),('n','nerul'),('p','parel'),('s','santacruz')])
-    price  = StringField('enter the price range(approx)', validators=[DataRequired()])
-    details  = StringField('details', validators=[DataRequired()])
-    accomodation = StringField('no of people accomodated')
-    contact  = StringField('contact', validators=[DataRequired()])
+    occasion  = StringField('Events hosted:')
+    location  = SelectField('Location:',choices=[('a','Andheri'),('z','any'),('b','Borivali'),('c','Churchgate'),('d','Dadar'),('g','Ghatkopar'),('k','Kandivali'),('n','Nerul'),('s','Santacruz')])
+    price  = StringField('Approximate Budget Limit:', validators=[DataRequired()])
+    details  = StringField('Details:', validators=[DataRequired()])
+    accomodation = StringField('Accomodation Capacity:')
+    contact  = StringField('Contact:', validators=[DataRequired()])
     address  = StringField('address')
-    #requirement =SelectField('requirement', validators=[DataRequired()],choices=[('h','halls')])
+    requirement =SelectField('requirement', validators=[DataRequired()],choices=[('h','halls')])
     submit = SubmitField('Sign Up')
 
 class dRegistrationForm(FlaskForm):
@@ -46,12 +46,13 @@ class dRegistrationForm(FlaskForm):
                                      validators=[EqualTo('password')])
     
     
-    price  = StringField('enter the price range')
+    price  = StringField('Approximate Budget Limit:', validators=[DataRequired()])
+ 
 
-    contact  = StringField('contact', validators=[DataRequired()])
-    details  = StringField('details', validators=[DataRequired()])
+    contact  = StringField('Contact:', validators=[DataRequired()])
+    details  = StringField('Details:', validators=[DataRequired()])
     
-    requirement =SelectField('requirement',
+    requirement =SelectField('Requirement:',
                         validators=[DataRequired()],choices=[('d','decorators')])
  
     submit = SubmitField('Sign Up')
@@ -64,10 +65,10 @@ class cRegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[EqualTo('password')])
     price  = StringField('enter the price range')
-    contact  = StringField('contact', validators=[DataRequired()])
-    requirement =SelectField('requirement',
-                        validators=[DataRequired()],choices=[('c','cateorors')])
-    details  = StringField('details', validators=[DataRequired()])
+    contact  = StringField('Contact:', validators=[DataRequired()])
+    requirement =SelectField('Requirement:',
+                        validators=[DataRequired()],choices=[('c','caterers')])
+    details  = StringField('Details:', validators=[DataRequired()])
     
     submit = SubmitField('Sign Up')
 
@@ -76,57 +77,49 @@ class cRegistrationForm(FlaskForm):
 
 
 class planRegistrationForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    email = StringField('Email address', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
+    name = StringField('Name:', validators=[DataRequired()])
+    email = StringField('Email address:', validators=[DataRequired(), Email()])
+    password = PasswordField('Password:', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password:',
                                      validators=[EqualTo('password')])
     
-    special=StringField('known for', validators=[DataRequired()])
-    details=StringField('details', validators=[DataRequired()])
-    requirement =SelectField('requirement',
+    special=StringField('Known for', validators=[DataRequired()])
+    details=StringField('Details:', validators=[DataRequired()])
+    requirement =SelectField('Requirement:',
                         choices=[('p','party planners')])
  
-    price  = StringField('enter the price range')
+    price  = StringField('Approximate Budget Limit:')
 
-    contact  = StringField('contact', validators=[DataRequired()])
+    contact  = StringField('Contact:', validators=[DataRequired()])
    
     submit = SubmitField('Sign Up')
 
    
 class uRegistrationForm(FlaskForm):
-    
-    #email = EmailField('Email address', validators=[DataRequired(), Email()])
-   
-    requirement  = SelectField('requirement',
+    requirement  = SelectField('Requirement:',
                         choices=[('h','halls'),('d','decorators'),('c','caterers'),('p','partyplanners')])
-    location  = SelectField('location',choices=[('a','andheri'),('z','any'),('b','borivali'),('c','churchgate'),('d','dadar'),('g','ghatkopar'),('j','jogeshwari'),('k','kandivali'),('l','lowerparel'),('n','nerul'),('p','parel'),('s','santacruz')])
+    location  = SelectField('Location:',choices=[('a','Andheri'),('z','any'),('b','Borivali'),('c','Churchgate'),('d','Dadar'),('g','Ghatkopar'),('k','Kandivali'),('n','Nerul'),('s','Santacruz')])    
     price  = StringField(' enter the maximum price you can go upto')
-   
+    submit = SubmitField('Search')
 
-    submit = SubmitField('see the match')
 
-    
-
-   
-                       
 class UpdateAccountForm(FlaskForm):
-    name = StringField(' hall name', validators=[DataRequired()])
-    email = EmailField('Email address', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    name = StringField(' Change name:', validators=[DataRequired()])
+    email = EmailField('Change Email address:', validators=[DataRequired(), Email()])
+    password = PasswordField('Change Password:', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[EqualTo('password')])
-    special=StringField('known for', validators=[DataRequired()])
+    special=StringField('Known for:', validators=[DataRequired()])
     
-    occasion  = StringField('space used for')
-    location  = SelectField('location',choices=[('a','andheri'),('z','any'),('b','borivali'),('c','churchgate'),('d','dadar'),('g','ghatkopar'),('j','jogeshwari'),('k','kandivali'),('l','lowerparel'),('n','nerul'),('p','parel'),('s','santacruz')])
+    occasion  = StringField('Events hosted:')
+    location  = SelectField('Location:',choices=[('a','Andheri'),('z','any'),('b','Borivali'),('c','Churchgate'),('d','Dadar'),('g','Ghatkopar'),('k','Kandivali'),('n','Nerul'),('s','Santacruz')])
     price  = StringField('enter the price range(approx)', validators=[DataRequired()])
-    details  = StringField('details', validators=[DataRequired()])
-    accomodation = StringField('no of people accomodated')
-    contact  = StringField('contact', validators=[DataRequired()])
-    address  = StringField('address')
+    details  = StringField('Details:', validators=[DataRequired()])
+    accomodation = StringField('Accomodation Capacity:')
+    contact  = StringField('Contact:', validators=[DataRequired()])
+    address  = StringField('Address:')
     
-    requirement  = SelectField('requirement',
+    requirement  = SelectField('Requirement:',
                         choices=[('h','halls'),('d','decorators'),('c','caterers'),('p','partyplanners')])
    
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def home():
         print("success1")
         print("user details stored") # this is not getting printed, for sure
         findd(flag)
-    return render_template('usermatch.html', title='home', form=form,flag=flag)
+    return render_template('home.html', title='home', form=form,flag=flag)
 
    
 @app.route("/findd/<int:flag>",methods=['GET', 'POST'])
@@ -211,7 +211,7 @@ def decd():
 @app.route("/catd")
 def catd():
     org = Org.query.filter_by(requirement='c').all()
-    return render_template('flashhall.html', org=org)
+    return render_template('flashCaterer.html', org=org)
 
 @app.route("/pd")
 def pd():
@@ -282,11 +282,8 @@ def decreg():
 @app.route("/catererreg", methods=['GET', 'POST'])
 def catererreg():
     form = cRegistrationForm()
-    print("hi palak going well")
     if form.validate_on_submit():
         print("validated")
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        
         org = Org(name=form.name.data,email=form.email.data,password=form.password.data,requirement='c',price=form.price.data,details=form.details.data,contact=form.contact.data)
         db.session.add(org)
         db.session.commit()
